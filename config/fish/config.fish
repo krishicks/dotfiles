3d31407e1f7d7a10b57375c57b31572467afb35b
Kris Hicks <krishicks@gmail.com>
2020-04-04 12:45:38 -0700

    Remove rust, rbenv, direnv config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,22 +5,10 @@ set -x PATH /usr/local/go/bin $PATH
 set -x GOPATH ~/go
 set -x PATH $GOPATH/bin $PATH
 
-# rust
-set -x PATH ~/.cargo/bin $PATH
-
-
 # git
 set -x GIT_EDITOR vim
-
-# rbenv
-set -x PATH $HOME/.rbenv/bin $PATH
 
 # fzf
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!{.git,go/pkg}/*" 2> /dev/null'
 
-# direnv
-eval (direnv hook fish)
-
-# rbenv
-status --is-interactive; and source (rbenv init -|psub)
 
